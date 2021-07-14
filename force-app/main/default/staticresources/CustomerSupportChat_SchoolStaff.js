f635357df59a92b96f1c005e2526b16f7914b5bf
salesforcean <salesforcean@gmail.com>
2021-07-14 10:52:32 +0530

    Changes in NESDEVOBL1 as of 20210713

--- force-app/main/default/staticresources/CustomerSupportChat_SchoolStaff.js
+++ force-app/main/default/staticresources/CustomerSupportChat_SchoolStaff.js
@@ -1,4 +1,4 @@
- window._snapinsSnippetSettingsFile = (function() {
+window._snapinsSnippetSettingsFile = (function() {
 //Swapna:For Customer Support Chat
 embedded_svc.snippetSettingsFile.directToButtonRouting = function(prechatFormData) {
 
@@ -16,7 +16,6 @@ embedded_svc.snippetSettingsFile.fallbackRouting = ['USERIDHERE', 'BUTTONIDHERE'
 embedded_svc.snippetSettingsFile.extraPrechatInfo = [{
 "entityName": "Contact",
 "linkToEntityName": "Case",
-"linkToEntityField": "Staff_Assignee__c",
 "linkToEntityField": "ContactId",
 "saveToTranscript": "ContactId",
 "entityFieldMaps" : [{
